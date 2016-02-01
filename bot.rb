f697df15b6866d62d7aaee7447248558b3d6326c
Raphael Tan <raphaeltanyw@gmail.com>
2016-02-01 14:29:46 +0800

    - Modify message formatting

--- bot.rb
+++ bot.rb
@@ -18,12 +18,16 @@ $bamboo_url = config["BAMBOO_URL"]
 plan_keys = config["PLAN_KEYS"]
 
 def broadcast(message)
-  Telegram::Bot::Client.run($token) do |bot|
-    bot.api.send_message(chat_id: $channel_id, text: message)
+  begin
+    Telegram::Bot::Client.run($token) do |bot|
+      bot.api.send_message(chat_id: $channel_id, text: message)
+    end
+  rescue Exception => e
+    p "#{Time.now} Failed to send message!"
   end
 end
 
-scheduler.every '1m' do
+scheduler.every '5s' do
   plan_keys.each do |plan|
     p plan
     response = HTTParty.get("#{$bamboo_url}/rest/api/latest/result/#{plan}.json?os_authType=basic", basic_auth: authinfo)
@@ -37,10 +41,10 @@ scheduler.every '1m' do
         author.children.text
       end
       p "failed"
-      broadcast("#{latest_build_json["plan"]["shortName"]} build #{latest_keys[plan]} failed! \nCulprits: #{culprits}")
+      broadcast("\xF0\x9F\x94\xA5[#{latest_build_json["plan"]["shortName"]}] build #{latest_keys[plan]} failed!\xF0\x9F\x94\xA5 \nCulprits: #{culprits}")
     elsif (latest_build_json["state"] == "Successful" && prev_buid_failed[plan] == true)
       p "passed"
-      broadcast("#{latest_build_json["plan"]["shortName"]} build #{latest_keys[plan]} passing again!")
+      broadcast("\xF0\x9F\x8D\x80[]#{latest_build_json["plan"]["shortName"]}] build #{latest_keys[plan]} passing again!\xF0\x9F\x8D\x80")
     end
 
     if(latest_build_json["state"] == "Successful")
