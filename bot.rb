e5b9e2b99ee7f5e490dd3ae951d5adb9771a09cd
Raphael Tan <raphaeltanyw@gmail.com>
2016-02-24 17:28:29 +0800

    Add build plan URL to broadcast message

--- bot.rb
+++ bot.rb
@@ -18,6 +18,7 @@ $bamboo_url = config["BAMBOO_URL"]
 plan_keys = config["PLAN_KEYS"]
 frequency = config["FREQUENCY"]
 
+
 def broadcast(message)
   backoff = 5
   loop do
@@ -50,7 +51,8 @@ scheduler.every frequency do
       indiv_response = HTTParty.get("#{$bamboo_url}/rest/api/latest/result/#{latest_keys[plan]}.json?os_authType=basic&expand=changes.change", basic_auth: authinfo)
       culprits = indiv_response["changes"]["change"].map { |commit| commit["author"] unless commit["comment"].start_with?("Merge pull request") }.compact.uniq.join(', ')
       p "failed"
-      broadcast("\xF0\x9F\x94\xA5[#{latest_build_json["plan"]["shortName"]}] build #{latest_keys[plan]} failed!\xF0\x9F\x94\xA5 \nCulprits: #{culprits}")
+      url = "#{$bamboo_url}/browse/#{plan}/latest"
+      broadcast("\xF0\x9F\x94\xA5[#{latest_build_json["plan"]["shortName"]}] build #{latest_keys[plan]} failed!\xF0\x9F\x94\xA5 \n#{url} \nCulprits: #{culprits}")
     elsif (latest_build_json["state"] == "Successful" && prev_buid_failed[plan] == true)
       p "passed"
       broadcast("\xF0\x9F\x8D\x80[#{latest_build_json["plan"]["shortName"]}] build #{latest_keys[plan]} passing again!\xF0\x9F\x8D\x80")
